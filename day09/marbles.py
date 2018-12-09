f343a79ae0efa7eaf41690e65ddc48f34a11b646
Khinshan Khan <khanshan501@gmail.com>
2018-12-09 15:21:55 -0500

    .

--- day09/marbles.py
+++ day09/marbles.py
@@ -1,7 +1,6 @@
 from collections import deque
 
-def play(max_players, beginning, end, circle):
-    scores = [0] * max_players
+def play(max_players, beginning, end, circle, scores):
     for marble in xrange(beginning, end + 1):
         if marble % 23 == 0:
             circle.rotate(7)
@@ -10,10 +9,9 @@ def play(max_players, beginning, end, circle):
         else:
             circle.rotate(-1)
             circle.append(marble)
-    return (circle, max(scores))
+    return (circle, max(scores), scores)
 
 raw = [l.rstrip().split(' ') for l in open("input.txt")][0]
-circle, score = play(int(raw[0]), 1, int(raw[6]), deque([0])) # part 1
-#circle, score2 = play(int(raw[0]), int(raw[6]) - 1, (int(raw[6]) * 100) + 1, circle) # part 2
-circle, score2 = play(int(raw[0]), 1, (int(raw[6]) * 100), deque([0])) # part 2
+circle, score, scores = play(int(raw[0]), 1, int(raw[6]), deque([0]), [0] * int(raw[0])) # part 1
+circle, score2, scores = play(int(raw[0]), int(raw[6]) + 1, (int(raw[6]) * 100), circle, scores) # part 2
 print score, score2
