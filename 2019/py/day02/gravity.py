1d5321b9b799887bb70da49a0bff4513e81664ac
Khinshan Khan <khanshan501@gmail.com>
2019-12-02 02:11:45 -0500

    cleaned code

--- 2019/py/day02/gravity.py
+++ 2019/py/day02/gravity.py
@@ -1,36 +1,67 @@
-def set(pos, listo):
-    if listo[pos] == 1:
-        listo[listo[pos + 3]] = listo[listo[pos + 1]] + listo[listo[pos + 2]]
-        return set(pos + 4, listo)
-    elif listo[pos] == 2:
-        listo[listo[pos + 3]] = listo[listo[pos + 1]] * listo[listo[pos + 2]]
-        return set(pos + 4, listo)
-    else:
+from operator import add, mul
+
+TARGET = 19690720
+OPCODES = {
+    1:add,
+    2:mul,
+    99: False
+}
+
+PART1_POS1 = 12
+PART1_POS2 = 2
+
+def program_alarm_1202(pos, listo):
+    # destructure instructions to 4 variables
+    opcode, op1, op2, nex = listo[pos: pos + 4]
+
+    # determine if opcode continues programs or halts
+    if not OPCODES[opcode]:
         return listo
+    else:
+        # execute instruction
+        listo[nex] = OPCODES[opcode](listo[op1], listo[op2])
+        # keep going until we reach a halt
+        return program_alarm_1202(pos + 4, listo)
 
 def gravity(f, val1, val2):
     for l in f:
         l = [int(i) for i in l.strip().split(",")]
-        ll = [x for x in l]
-        l[1] = 12
-        l[2] = 2
-        l = set(0, l)
-        val1 = l[0]
 
-        for i in range(100):
-            for j in range(100):
-                lll = [x for x in ll]
-                lll[1] = i
-                lll[2] = j
-                lll = set(0, lll)
-                if lll[0] == 19690720:
-                    val2 = (100 * i) + j
+        # Try all (noun, verb) until we found a good match
+        for noun in range(100):
+            found = False
+
+            for verb in range(100):
+                # make a copy of list
+                ll = [x for x in l]
+                # set the initial states
+                ll[1] = noun
+                ll[2] = verb
+                # run stateful changes on list
+                ll = program_alarm_1202(0, ll)
+
+                # a safe guess that program hasn't ended yet
+                # really a heuristic based on my input
+                if noun == PART1_POS1 and verb == PART1_POS2:
+                    # part 1 complete
+                    val1 = ll[0]
+                if ll[0] == TARGET:
+                    # pair works for part 2
+                    # part 2 complete
+                    val2 = (100 * noun) + verb
+                    # break double for loop
+                    found = True
                     break
+
+            if found:
+                break
+
     return (val1, val2)
 
 
 def main():
     f = open("input.txt")
+    # print part and part 2
     print(gravity(f, 0, 0))
     f.close()
 
