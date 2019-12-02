7186eaa22ed381e7b63e3fc9760d9f396f7439a6
Khinshan Khan <khanshan501@gmail.com>
2019-12-01 22:53:26 -0500

    secure io

--- 2019/py/day01/fuel.py
+++ 2019/py/day01/fuel.py
@@ -1,10 +1,11 @@
 def fuel(sum1, sum2):
-    for l in open("input.txt"):
-        t = int(l)
-        sum1 += t // 3 - 2
-        while (t // 3 - 2) > 0:
-            sum2 += t // 3 - 2
-            t = t // 3 - 2
+    with f as open("input.txt"):
+        for l in f:
+            t = int(l)
+            sum1 += t // 3 - 2
+            while (t // 3 - 2) > 0:
+                sum2 += t // 3 - 2
+                t = t // 3 - 2
     return (sum1, sum2)
 
 print(fuel(0, 0))
