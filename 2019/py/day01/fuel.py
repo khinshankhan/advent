07e62bac0a9d3ada1aa3014f02d9b63b6104a230
Khinshan Khan <khanshan501@gmail.com>
2019-12-01 23:00:20 -0500

    a little bit more boilerplated

--- 2019/py/day01/fuel.py
+++ 2019/py/day01/fuel.py
@@ -1,11 +1,18 @@
-def fuel(sum1, sum2):
-    with f as open("input.txt"):
-        for l in f:
-            t = int(l)
-            sum1 += t // 3 - 2
-            while (t // 3 - 2) > 0:
-                sum2 += t // 3 - 2
-                t = t // 3 - 2
+def fuel(f, sum1, sum2):
+    for l in f:
+        t = int(l)
+        sum1 += t // 3 - 2
+        while (t // 3 - 2) > 0:
+            sum2 += t // 3 - 2
+            t = t // 3 - 2
     return (sum1, sum2)
 
-print(fuel(0, 0))
+
+def main():
+    f = open("input.txt")
+    print(fuel(f, 0, 0))
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
