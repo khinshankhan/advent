eea3cfa7a695e42492f94ca2d387a6dbc1a2e24b
Khinshan Khan <khanshan501@gmail.com>
2020-12-01 01:22:32 -0500

    day 1 go

---
+++ 2020/go/cmds/day01/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kkhan01/advent/2020/go/utils"
+)
+
+func part1(nums []int) (int, int) {
+	for i, e := range nums {
+		for _, e2 := range nums[i : len(nums)-1] {
+			if e+e2 == 2020 {
+				return e, e2
+			}
+		}
+	}
+
+	return -1, -1
+}
+
+func part2(nums []int) (int, int, int) {
+	for i, e := range nums {
+		for j, e2 := range nums[i : len(nums)-1] {
+			for _, e3 := range nums[j : len(nums)-1] {
+				if e+e2+e3 == 2020 {
+					return e, e2, e3
+				}
+			}
+		}
+	}
+
+	return -1, -1, -1
+}
+
+func main() {
+	nums, err := utils.ReadIntFile("day1.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	e, e2 := part1(nums)
+	fmt.Println(e*e2, e, e2)
+	e, e2, e3 := part2(nums)
+	fmt.Println(e*e2*e3, e, e2, e3)
+}
