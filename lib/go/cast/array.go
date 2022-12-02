28610fdb31697cc219fda513a8f15751c856099d
Khinshan Khan <shan@uptogood.dev>
2022-12-02 16:08:57 -0500

    casting between common data types

---
+++ lib/go/cast/array.go
@@ -0,0 +1,25 @@
+package cast
+
+import "strconv"
+
+func From1DStringTo1DInt(input []string) []int {
+	ret := make([]int, len(input))
+	for j, e := range input {
+		num, err := strconv.Atoi(e)
+		if err != nil {
+			panic(err)
+		}
+		ret[j] = num
+	}
+
+	return ret
+}
+
+func From2DStringTo2DInt(input [][]string) [][]int {
+	ret := make([][]int, len(input))
+	for i, row := range input {
+		ret[i] = From1DStringTo1DInt(row)
+	}
+
+	return ret
+}
