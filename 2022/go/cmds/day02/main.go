e0bc1d559ea3d179089dd82bdac29b5b76e458c3
Khinshan Khan <shan@uptogood.dev>
2022-12-02 16:53:22 -0500

    day 2 part b

--- 2022/go/cmds/day02/main.go
+++ 2022/go/cmds/day02/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 
 	"github.com/khinshankhan/advent/lib/go/io"
+	"github.com/khinshankhan/advent/lib/go/util"
 )
 
 func main() {
 	input := io.Read2DString("../data/day02.txt", "\n", " ", true)
 	parta(input)
+	partb(input)
 }
 
 var winningHand = map[string]string{
@@ -39,6 +41,33 @@ func parta(input [][]string) {
 	sum := 0
 	for _, hands := range input {
 		opp, mine := convertOpp[hands[0]], convertMine[hands[1]]
+		sum += handPoints[mine]
+		if opp == mine {
+			sum += 3
+		} else if winningHand[mine] == opp {
+			sum += 6
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func partb(input [][]string) {
+	losingHand := util.ReverseMap(winningHand)
+
+	sum := 0
+	for _, hands := range input {
+		opp, scenario := convertOpp[hands[0]], hands[1]
+		mine := ""
+		switch scenario {
+		case "X":
+			mine = winningHand[opp]
+		case "Y":
+			mine = opp
+		case "Z":
+			mine = losingHand[opp]
+		}
+
 		sum += handPoints[mine]
 		if opp == mine {
 			sum += 3
