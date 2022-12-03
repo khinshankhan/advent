6d85e5ce013c71d2c70c6328305393bed04e5c2f
Khinshan Khan <shan@uptogood.dev>
2022-12-03 12:12:47 -0500

    more common iterable functions

--- lib/go/util/array.go
+++ lib/go/util/array.go
@@ -7,3 +7,41 @@ func Sum1DInt(input []int) int {
 	}
 	return sum
 }
+
+func Intersection[T comparable](a, b []T) []T {
+	intersection := []T{}
+
+	aCounts := make(map[T]int)
+	for _, e := range a {
+		if count, ok := aCounts[e]; ok {
+			aCounts[e] = count + 1
+		} else {
+			aCounts[e] = 1
+		}
+	}
+
+	for _, e := range b {
+		if count, ok := aCounts[e]; ok && count > 0 {
+			intersection = append(intersection, e)
+			aCounts[e] = count - 1
+		}
+	}
+
+	return intersection
+}
+
+func Unique[T comparable](list []T) []T{
+	m  := make(map[T]bool)
+	for _, e := range list {
+		m[e] = true
+	}
+
+	ret := make([]T, len(m))
+	i:= 0
+	for k := range m{
+		ret[i] = k
+		i++
+	}
+
+	return ret
+}
