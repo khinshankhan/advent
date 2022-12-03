7713cfbad07cfd38f8bd073d0ed11870d77d0f47
Khinshan Khan <shan@uptogood.dev>
2022-12-03 12:13:23 -0500

    day 3 part a

---
+++ 2022/go/cmds/day03/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/khinshankhan/advent/lib/go/io"
+	"github.com/khinshankhan/advent/lib/go/util"
+)
+
+func main() {
+	input := io.Read1DString("../data/day03.txt", "\n", true)
+	parta(input)
+}
+
+func runePoint(r rune) int {
+	if r >= 'a' {
+		return int(r-'a') + 1
+	}
+	return int(r-'A') + 27
+}
+
+func splitStringEvenly(s string) (string, string) {
+	midpoint := len(s) / 2
+	return s[:midpoint], s[midpoint:]
+}
+
+func parta(input []string) {
+	points := 0
+	for _, rucksack := range input {
+		a, b := splitStringEvenly(rucksack)
+		intersection := util.Intersection([]rune(a), []rune(b))
+		for _, r := range util.Unique(intersection) {
+			points += runePoint(r)
+		}
+	}
+	fmt.Println(points)
+}
