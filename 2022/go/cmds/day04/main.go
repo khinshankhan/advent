e633e26f859caba05fce7552d2e605df7cafa4b7
Khinshan Khan <shan@uptogood.dev>
2022-12-04 00:29:18 -0500

    day 4 part b

--- 2022/go/cmds/day04/main.go
+++ 2022/go/cmds/day04/main.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	input := io.Read2DString("../data/day04.txt", "\n", ",", true)
 	parta(input)
+	partb(input)
 }
 
 func parta(input [][]string) {
@@ -19,6 +20,18 @@ func parta(input [][]string) {
 		firstStr, secondStr := strings.Split(pair[0], "-"), strings.Split(pair[1], "-")
 		first, second := cast.From1DStringTo1DInt(firstStr), cast.From1DStringTo1DInt(secondStr)
 		if (first[0] <= second[0] && first[1] >= second[1]) || (second[0] <= first[0] && second[1] >= first[1]) {
+			contained += 1
+		}
+	}
+	fmt.Println(contained)
+}
+
+func partb(input [][]string) {
+	contained := 0
+	for _, pair := range input {
+		firstStr, secondStr := strings.Split(pair[0], "-"), strings.Split(pair[1], "-")
+		first, second := cast.From1DStringTo1DInt(firstStr), cast.From1DStringTo1DInt(secondStr)
+		if (first[0] <= second[0] && first[1] >= second[0]) || (second[0] <= first[0] && second[1] >= first[0]) {
 			contained += 1
 		}
 	}
