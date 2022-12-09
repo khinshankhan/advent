a50607b78ad7165f8d20eb8a02c681b17b7f30fd
Khinshan Khan <shan@uptogood.dev>
2022-12-09 18:34:14 -0500

    clean up day 9

--- 2022/go/cmds/day09/main.go
+++ 2022/go/cmds/day09/main.go
@@ -11,32 +11,11 @@ import (
 	"github.com/khinshankhan/advent/lib/go/util"
 )
 
-var sample1 = `R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2`
-
-var sample2 = `R 5
-U 8
-L 8
-D 3
-R 17
-D 10
-L 25
-U 20`
-
 func main() {
 	s := io.ReadRelativeFile("../data/day09.txt")
-	// s := sample1
 	input := parse(s)
-	fmt.Println(parta(input))
 
-	// s = sample2
-	// input = parse(s)
+	fmt.Println(parta(input))
 	fmt.Println(partb(input))
 }
 
@@ -52,32 +31,23 @@ var moves = map[string]image.Point{
 }
 
 func parta(input [][]string) int {
-	h, t := image.Point{0, 0}, image.Point{0, 0}
-	set := ds.NewSet(math.ClonePoint(t))
-	for _, line := range input {
-		d, n := line[0], util.FromStringToInt(line[1])
-		for m := 0; m < n; m++ {
-			prevH := math.ClonePoint(h)
-			h = h.Add(moves[d])
-			if math.ChebyshevDistance(h, t) > 1 {
-				t = prevH
-				set.Add(math.ClonePoint(t))
-			}
-		}
-	}
-
-	return set.Len()
+	return calcUniqueTailPositions(2, input)
 }
 
 func partb(input [][]string) int {
-	rope := make([]image.Point, 10)
+	return calcUniqueTailPositions(10, input)
+}
+
+func calcUniqueTailPositions(ropeLength int, input [][]string) int {
+	rope := make([]image.Point, ropeLength)
 	set := ds.NewSet(math.ClonePoint(image.Point{}))
 	for _, line := range input {
 		d, n := line[0], util.FromStringToInt(line[1])
 		for m := 0; m < n; m++ {
-			rope[0] = rope[0].Add(moves[d])
-			for i := 1; i < len(rope); i++ {
-				if math.ChebyshevDistance(rope[i-1], rope[i]) > 1 {
+			for i := 0; i < len(rope); i++ {
+				if i == 0 {
+					rope[0] = rope[0].Add(moves[d])
+				} else if math.ChebyshevDistance(rope[i-1], rope[i]) > 1 {
 					rope[i] = math.ChebyshevMove(rope[i], rope[i-1])
 				}
 			}
