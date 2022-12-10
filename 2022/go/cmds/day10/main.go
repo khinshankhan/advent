562156ba89801ca6d718e82a9add9b047d83248e
Khinshan Khan <shan@uptogood.dev>
2022-12-10 02:11:33 -0500

    clean up with vm tick fn

--- 2022/go/cmds/day10/main.go
+++ 2022/go/cmds/day10/main.go
@@ -22,53 +22,60 @@ func parse(s string) [][]string {
 	return util.TransformString2D(strings.TrimSpace(s), "\n", " ", true, true)
 }
 
-var moves = map[string]int{
-	"addx": 2,
-	"noop": 1,
-}
+func parta(input [][]string) (interesting int) {
+	interestingCyles := ds.NewSet(20, 60, 100, 140, 180, 220)
 
-func parta(input [][]string) int {
 	cycle := 0
 	strength := 1
-	interestingCyles := ds.NewSet(20, 60, 100, 140, 180, 220)
-	interesting := []int{}
-	for _, l := range input {
-		// prob smarter way
-		for j := 0; j < moves[l[0]]; j++ {
-			cycle += 1
-			if interestingCyles.Has(cycle) {
-				interesting = append(interesting, cycle*strength)
-			}
-		}
-		if len(l) > 1 {
-			strength += util.FromStringToInt(l[1])
+	tick := func() {
+		cycle += 1
+		if interestingCyles.Has(cycle) {
+			interesting += cycle * strength
 		}
 	}
-	return math.SumNums(interesting...)
+
+	for _, l := range input {
+		switch l[0] {
+		case "addx":
+			tick()
+			tick()
+			strength += util.FromStringToInt(l[1])
+		case "noop":
+			tick()
+		}
+	}
+
+	return interesting
 }
 
 func partb(input [][]string) string {
-
 	msg := ""
+
 	cycle := 0
-	pos := 2
-	for _, l := range input {
-		// prob smarter way
-		for j := 0; j < moves[l[0]]; j++ {
-			cycle += 1
-			crtpos := cycle % 40
-			if crtpos == 1 && cycle != 1 {
-				msg += "\n"
-			}
-			if crtpos == pos || crtpos == pos+1 || crtpos == pos-1 {
-				msg += "#"
-			} else {
-				msg += "."
-			}
+	sprite := 2
+	tick := func() {
+		cycle += 1
+		crtpos := cycle % 40
+		if crtpos == 1 && cycle != 1 {
+			msg += "\n"
 		}
-		if len(l) > 1 {
-			pos += util.FromStringToInt(l[1])
+		if math.Abs(sprite-crtpos) <= 1 {
+			msg += "#"
+		} else {
+			msg += "."
 		}
 	}
+
+	for _, l := range input {
+		switch l[0] {
+		case "addx":
+			tick()
+			tick()
+			sprite += util.FromStringToInt(l[1])
+		case "noop":
+			tick()
+		}
+	}
+
 	return msg
 }
